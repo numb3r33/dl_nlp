f3487a7f38811cbb699d1883fcff29314c924137
abhishek <abhicoders@gmail.com>
2019-03-09 00:08:06 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Research and Experiments in Natural Language Processing.',
+    author='Abhishek Sharma',
+    license='MIT',
+)
