23540002c39fe6273164f9209ec69ad33762826a
numb3r33 <abhicoders@gmail.com>
2019-03-08 21:08:32 +0000

    Define model.

---
+++ src/word2vec/model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import config
+
+
+def create_model(V):
+    model = nn.Sequential(nn.Embedding(V, config.EMBEDDING_SIZE),
+                          nn.Linear(config.EMBEDDING_SIZE, V)
+            ).cuda()
+
+    return model
