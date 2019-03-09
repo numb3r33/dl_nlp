9fdf1c07825ed25f32c37d6bd696f94af4501df5
numb3r33 <abhicoders@gmail.com>
2019-03-09 08:59:30 +0000

    Plot 2-d embeddings.

---
+++ src/word2vec/single_matrix.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+from utils import *
+from train import *\
+
+def main(data_path):
+    text = read_data(data_path)
+    tokenized_text = tokenize(text)
+    vocabulary     = build_vocabulary(tokenized_text)
+
+    word2index, index2word  = numericalization(vocabulary)
+    contexts                = build_contexts(tokenized_text, config.WINDOW_SIZE, word2index)
+    V                       = len(word2index.keys())
+    
+    print('sample of some words: {}'.format(np.random.choice(list(word2index.keys()), 5)))
+    model = train_w2v_one_matrix(contexts, V, num_skips=config.NUM_SKIPS, batch_size=config.BATCH_SIZE)
+    embeddings = get_embedding(model)
+    print(most_similar(embeddings, index2word, word2index, config.WORD))
+
+    visualize_embeddings(embeddings, index2word, config.SAMPLE)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Word2Vec Processing')
+
+    parser.add_argument('-data_path', help='Path to the data')
+    args = parser.parse_args()
+    data_path = args.data_path
+    
+    main(data_path)
---
+++ src/word2vec/visualization.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+from MulticoreTSNE import MulticoreTSNE as TSNE
+from sklearn.preprocessing import scale
+
+import config
+
+def draw_vectors(x, y, word_texts):
+    fig, ax = plt.subplots(figsize=(14, 8))
+    ax.scatter(x, y)
+    
+    for i, txt in enumerate(word_texts):
+        ax.annotate(txt, (x[i], y[i]))
+
+    fig.savefig(config.PROJECTION_FILE)
+
+
+
+def get_tsne_projection(word_vectors):
+    tsne = TSNE(n_components=2)
+    return scale(tsne.fit_transform(word_vectors))
+
+
+def visualize_embeddings(embeddings, index2word, word_count):
+    word_vectors = embeddings[1: word_count + 1]
+    words        = index2word[1: word_count + 1]
+
+    word_tsne    = get_tsne_projection(word_vectors)
+    draw_vectors(word_tsne[:, 0], word_tsne[:, 1], words)
