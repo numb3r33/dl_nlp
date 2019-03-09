8c655241388c83ae8da273e7ef5ef7ad3eb38901
numb3r33 <abhicoders@gmail.com>
2019-03-09 17:43:11 +0000

    Add new configs for vanilla neg sampling.

--- src/word2vec/config.py
+++ src/word2vec/config.py
@@ -1,11 +1,12 @@
-MIN_COUNT    = 5
-WINDOW_SIZE  = 2
-LEARNING_RATE = .025
-LOGGING_STEPS = 1000
-NUM_SKIPS     = 2
-BATCH_SIZE    = 2 ** 2
-EMBEDDING_SIZE = 32
-WORD           = 'eight'
-SAMPLE         = 1000
-PROJECTION_FILE= '../../references/tsne_projection.png'
-
+MIN_COUNT       = 5
+WINDOW_SIZE     = 2
+LEARNING_RATE   = .025
+LOGGING_STEPS   = 1000
+NUM_SKIPS       = 2
+BATCH_SIZE      = 2 ** 2
+EMBEDDING_SIZE  = 32
+WORD            = 'eight'
+SAMPLE          = 1000
+PROJECTION_FILE = '../../references/tsne_projection.png'
+NUM_NEG_WORDS   = 5
+UNIGRAM_POWER   = (3 / 4)
