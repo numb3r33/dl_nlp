03e99b8a69ef846eae3400c9ab5d8f678203b340
numb3r33 <abhicoders@gmail.com>
2019-04-22 14:14:28 +0000

    Separate configs based on experiment name.

--- src/cnn_sent_classification/exp_config.py
+++ src/cnn_sent_classification/exp_config.py
@@ -1,23 +1,26 @@
 PAD_TOKEN = 'xxxpad'
+UNK_TOKEN = 'xxxunk'
 W2V_PATH  = '../../data/processed/word2vec.bin.gz'
 FASTTEXT_PATH =  '../../data/processed/crawl-300d-2M.vec'
 TEXT_COL   = 'comment_text'
 LABEL_COLS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
 config = {
-    'lr': 1e-2,
-    'sent_len': 200,
-    'emb_type': 'glove',
-    #'emb_type': 'fasttext',
-    'dropout': 0.1,
-    'spatial_dropout': 0.4,
-    'max_vocab' : 100000,
-    'min_freq'  : 3,
-    'embed_size' : 300,
-    'valid_pct'  : 0.2,
-    'n_filters': [32, 32, 32, 32],
-    'filter_size': [1, 2, 3, 5],
-    'ncpus': 8,
-    'batch_size': 512,
-    'N_EPOCHS': 2
+    'simple_cnn': {
+        'lr': 1e-3,
+        'sent_len': 200,
+        'emb_type': 'fasttext',
+        #'emb_type': 'fasttext',
+        'dropout': 0.2,
+        'spatial_dropout': 0.4,
+        'max_vocab' : 100000,
+        'min_freq'  : 3,
+        'embed_size' : 300,
+        'valid_pct'  : 0.0,
+        'n_filters': [100, 100, 100],
+        'filter_size': [3, 4, 5],
+        'ncpus': 8,
+        'batch_size': 50,
+        'N_EPOCHS': 2
+         }
     }
