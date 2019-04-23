e34514a1c69a5c95ff54109fc81719cc6bdb9f09
numb3r33 <abhicoders@gmail.com>
2019-04-23 11:23:20 +0000

    Modify experiment configurations.

--- src/cnn_sent_classification/exp_config.py
+++ src/cnn_sent_classification/exp_config.py
@@ -10,17 +10,16 @@ config = {
         'lr': 1e-3,
         'sent_len': 200,
         'emb_type': 'fasttext',
-        #'emb_type': 'fasttext',
-        'dropout': 0.2,
-        'spatial_dropout': 0.4,
+        'dropout': 0.3,
+        'spatial_dropout': 0.3,
         'max_vocab' : 100000,
         'min_freq'  : 3,
         'embed_size' : 300,
-        'valid_pct'  : 0.0,
+        'valid_pct'  : 0.2,
         'n_filters': [100, 100, 100],
         'filter_size': [3, 4, 5],
         'ncpus': 8,
-        'batch_size': 50,
+        'batch_size': 128,
         'N_EPOCHS': 2
          }
     }
