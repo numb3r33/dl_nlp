383002b1932c55dedf7bade9d3a986d013764f74
numb3r33 <abhicoders@gmail.com>
2019-04-23 11:22:48 +0000

    Add Spatial Dropout.

--- src/cnn_sent_classification/models/simple_cnn.py
+++ src/cnn_sent_classification/models/simple_cnn.py
@@ -9,6 +9,16 @@ import torch.optim as optim
 from torch.utils.data import Dataset
 from pathlib import Path
 
+
+class SpatialDropout(nn.Dropout2d):
+    def forward(self, x):
+        x = x.permute(0, 3, 1, 2)
+        x = super(SpatialDropout, self).forward(x)
+        x = x.permute(0, 2, 3, 1)
+
+        return x
+
+
 class SimpleCNN(nn.Module):
     def __init__(self, emb_matrix, vocab_size, embed_size, config, pad_idx):
         super(SimpleCNN, self).__init__()
@@ -16,7 +26,6 @@ class SimpleCNN(nn.Module):
         # make sure embedding are non-trainable
         self.embedding        = nn.Embedding(vocab_size, embed_size, padding_idx=pad_idx)
         self.embedding.weight = nn.Parameter(emb_matrix)
-        
 
         self.n_filters    = config['n_filters']
         self.filter_sizes = config['filter_size']
@@ -29,7 +38,7 @@ class SimpleCNN(nn.Module):
         
         self.relu     = nn.ReLU()
         self.dropout  = nn.Dropout(config['dropout'])
-        self.spatial_dropout = nn.Dropout2d(config['spatial_dropout'])
+        self.spatial_dropout = SpatialDropout(config['spatial_dropout'])
         self.fc       = nn.Linear(np.sum(self.n_filters), 6)
         
     def forward(self, x):
@@ -40,7 +49,7 @@ class SimpleCNN(nn.Module):
    
         # spatial dropout on channels
         embed    = self.spatial_dropout(embed)
-        
+       
         conved = [self.relu(conv(embed)).squeeze(3) for conv in self.convs]
             
         #conv_n = [batch size, n_filters, sent len - filter_sizes[n]]        
